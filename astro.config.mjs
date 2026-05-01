793aecb9118acdd8b0ba561d1f4ccc6d38542254
Krish Vipin <krishvipin13@gmail.com>
2026-05-01 16:20:22 +0530

    Added vercel adapter to resolve path issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://www.astroanimate.com',
 	integrations: [sitemap()],
 	trailingSlash: "always",
+	adapter: vercel(),
 	build: {
 		format: 'directory',
 	},
